c50ec6d62c7803d12473ff1babb971da6b112fa6
Elin <elinhedinlarsson3@gmail.com>
2026-05-04 17:10:16 +0200

    Dog game with API

--- src/App.jsx
+++ src/App.jsx
@@ -21,34 +21,36 @@ function App() {
  return (
   <div>
     <h1>Dog Game</h1>
+  <button onClick={() =>
+  setSeen(seen + 1)}>Seen</button>
 
-    <button onClick={() => setSeen (seen + 1)}>
-      
+  <button onClick={() =>
+  setNotSeen(notSeen + 1)}>Not seen</button>
 
-      Seen
-    </button>
-
-    <button onClick={() => setNotSeen (seen + 1)}>
-      Not Seen
-    </button>
+  <button onClick={() =>{
+    fetch ("https://dog.ceo/api/breeds/image/random")
+    .then((res)=>res.json())
+    .then((data)=>
+      setDog(data.message));
+    }}>New Dog</button>
 
     <button onClick={() => {
-      fetch ("https://dog.ceo/api/breeds/image/random")
-      .then((res) => res.json())
-      .then((data) =>
-        setDog (data.message));
-      }}>
-        New Dog
-      </button>
+      setSeen(0);
+      setNotSeen(0);
+      }}>Reset</button>
 
-      <p>Seen:{seen}</p>
-      <p> notSeen:{notSeen}</p>
-      
-    
-    {dog && <img src={dog} alt ="dog" 
-    width="300" />}
-     </div>
+
+
+
+
+    <p>Seen: {seen}</p>
+    <p>Not seen: {notSeen}</p>
+
+    {dog && <img src={dog} alt ="dog" widht ="500" />}
+    </div>
  );
  }
- export default App;
 
+export default App;
+
+      
