9d4c4e1893c325e7ffcbb96055bae090d2f4adc8
Elin <elinhedinlarsson3@gmail.com>
2026-05-04 14:13:49 +0200

    first commit, setup api project

---
+++ src/pages/ApiPage.jsx
@@ -0,0 +1,14 @@
+import ApiPage from "./pages/ApiPage";
+
+function ApiPage() {
+    return (
+        <div>
+            <h1>API page</h1>
+            <p>Here I will fetch data from an API </p>
+        </div>
+
+
+
+    );
+}
+export default ApiPage;
