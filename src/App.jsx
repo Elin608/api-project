3ba9953781577426766f8814003ebe7773428e28
Elin <elinhedinlarsson3@gmail.com>
2026-05-05 13:18:42 +0200

    Added styling and confetti effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import { useEffect, useState }
  from "react";
+import confetti from "canvas-confetti";
 
 function App() {
+  const[feedback, setFeedback] =
+  useState ("");
   const[dog,setDog] =  useState ("");
   const [seen, setSeen] =useState(0);
   const [notSeen, setNotSeen] =useState(0);
+
+  const getDog =() => {
+    fetch ("https://dog.ceo/api/breeds/image/random")
+     .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      setDog(data.message);
+
+  const [feedback, setFeedback]=
+  useState("");
+
+ });
+ };
+
 
  
   useEffect (() => {
@@ -19,26 +36,39 @@ function App() {
  
 
  return (
-  <div>
-    <h1>Dog Game</h1>
-  <button onClick={() =>
-  setSeen(seen + 1)}>Seen</button>
+<div className="game">
+   <h1>Dog Game</h1>
 
-  <button onClick={() =>
-  setNotSeen(notSeen + 1)}>Not seen</button>
+  <button onClick={() => {
+  setSeen(seen + 1);
 
-  <button onClick={() =>{
-    fetch ("https://dog.ceo/api/breeds/image/random")
-    .then((res)=>res.json())
-    .then((data)=>
-      setDog(data.message));
-    }}>New Dog</button>
+  confetti({
+    particleCount: 80,
+    spread: 70,
+    origin: { y: 0.6}
+    });
+
+  getDog();
+  }}>
+  Seen
+  </button>
+
+  <button onClick={() => {
+  setNotSeen(notSeen + 1);
+  getDog();
+  }}>
+
+  Not seen
+  </button>
+
+
 
     <button onClick={() => {
       setSeen(0);
       setNotSeen(0);
       }}>Reset</button>
-
+      
+      <p>{feedback}</p>
 
 
 
