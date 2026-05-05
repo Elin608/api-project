fde0d10d5341673f190f92aacb78fa22456956ad
Elin <elinhedinlarsson3@gmail.com>
2026-05-05 14:53:52 +0200

    moved game to ApiPage and fixed structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,10 @@
-import { useEffect, useState }
- from "react";
-import confetti from "canvas-confetti";
+import ApiPage from "./pages/ApiPage";
+
 
 function App() {
-  const[feedback, setFeedback] =
-  useState ("");
-  const[dog,setDog] =  useState ("");
-  const [seen, setSeen] =useState(0);
-  const [notSeen, setNotSeen] =useState(0);
 
-  const getDog =() => {
-    fetch ("https://dog.ceo/api/breeds/image/random")
-     .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      setDog(data.message);
-
-  const [feedback, setFeedback]=
-  useState("");
-
- });
- };
-
-
- 
-  useEffect (() => {
-    fetch ("https://dog.ceo/api/breeds/image/random")
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      setDog(data.message);
-
- });
- }, []);
- 
-
- return (
-<div className="game">
-   <h1>Dog Game</h1>
-
-  <button onClick={() => {
-  setSeen(seen + 1);
-
-  confetti({
-    particleCount: 80,
-    spread: 70,
-    origin: { y: 0.6}
-    });
-
-  getDog();
-  }}>
-  Seen
-  </button>
-
-  <button onClick={() => {
-  setNotSeen(notSeen + 1);
-  getDog();
-  }}>
-
-  Not seen
-  </button>
-
-
-
-    <button onClick={() => {
-      setSeen(0);
-      setNotSeen(0);
-      }}>Reset</button>
-      
-      <p>{feedback}</p>
-
-
-
-
-    <p>Seen: {seen}</p>
-    <p>Not seen: {notSeen}</p>
-
-    {dog && <img src={dog} alt ="dog" widht ="500" />}
-    </div>
- );
- }
+  return <ApiPage />;
+}
 
 export default App;
 
-      
--- src/pages/ApiPage.jsx
+++ src/pages/ApiPage.jsx
@@ -1,14 +1,75 @@
-import ApiPage from "./pages/ApiPage";
+import { useEffect, useState }
+    from "react";
+import confetti from "canvas-confetti";
 
-function ApiPage() {
+function App() {
+    const [feedback, setFeedback] =
+        useState("");
+    const [dog, setDog] = useState("");
+    const [seen, setSeen] = useState(0);
+    const [notSeen, setNotSeen] = useState(0);
+
+    const getDog = async () => {
+        const res = await
+            fetch("https://dog.ceo/api/breeds/image/random")
+        const data = await res.json();
+        console.log(data);
+        setDog(data.message);
+    };
+
+
+
+    useEffect(() => {
+        getDog();
+    }, []);
+
+
+
     return (
-        <div>
-            <h1>API page</h1>
-            <p>Here I will fetch data from an API </p>
+        <div className="game">
+            <h1>Dog Game</h1>
+
+            <button onClick={() => {
+                setSeen(seen + 1);
+
+                confetti({
+                    particleCount: 80,
+                    spread: 70,
+                    origin: { y: 0.6 }
+                });
+
+                getDog();
+            }}>
+                Seen
+            </button>
+
+            <button onClick={() => {
+                setNotSeen(notSeen + 1);
+                getDog();
+            }}>
+
+                Not seen
+            </button>
+
+
+
+            <button onClick={() => {
+                setSeen(0);
+                setNotSeen(0);
+            }}>Reset</button>
+
+            <p>{feedback}</p>
+
+
+
+
+            <p>Seen: {seen}</p>
+            <p>Not seen: {notSeen}</p>
+
+            {dog && <img src={dog} alt="dog" widht="500" />}
         </div>
-
-
-
     );
 }
-export default ApiPage;
+
+export default App;
+
